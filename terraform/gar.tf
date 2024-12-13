64b4c3f5f569613cd69068c79b40e165cfab82e7
arsalen021 <arsalen021@gmail.com>
2024-12-13 17:52:26 +0100

    wip: add argocd service account key for repo server

--- terraform/gar.tf
+++ terraform/gar.tf
@@ -5,8 +5,28 @@ resource "google_artifact_registry_repository" "docker_dev" {
   format        = "DOCKER"
 }
 
+resource "google_artifact_registry_repository" "helm_dev" {
+  location      = var.region
+  repository_id = "helm-development-repository"
+  description   = "Helm development repository"
+  format        = "DOCKER"
+}
+
 resource "kubernetes_namespace" "jenkins" {
   metadata {
     name = "jenkins"
   }
+}
+
+resource "kubernetes_secret" "argocd" {
+  metadata {
+    name      = "gar-json-key"
+    namespace = "argocd"
+  }
+
+  data = {
+    "key.json" = base64encode(google_service_account_key.argocd.private_key)
+  }
+
+  type = "Opaque"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ data "google_project" "assignment" {
 locals {
   external_dns = "principal://iam.googleapis.com/projects/${data.google_project.assignment.number}/locations/global/workloadIdentityPools/${var.project}.svc.id.goog/subject/ns/external-dns/sa/external-dns"
   jenkins = "principal://iam.googleapis.com/projects/${data.google_project.assignment.number}/locations/global/workloadIdentityPools/${var.project}.svc.id.goog/subject/ns/jenkins/sa/jenkins"
+  argocd = "principal://iam.googleapis.com/projects/${data.google_project.assignment.number}/locations/global/workloadIdentityPools/${var.project}.svc.id.goog/subject/ns/argocd/sa/argocd-server"
 }
 
 resource "google_project_iam_member" "external_dns" {
@@ -23,7 +24,17 @@ resource "google_project_iam_member" "jenkins" {
   project = var.project
 }
 
-resource "google_dns_managed_zone_iam_member" "member" {
+resource "google_project_iam_member" "argocd" {
+  for_each = toset([
+    "roles/storage.admin",
+    "roles/artifactregistry.writer"
+  ])
+  role = each.key
+  member  = local.argocd
+  project = var.project
+}
+
+resource "google_dns_managed_zone_iam_member" "external_dns" {
   depends_on = [ google_dns_managed_zone.personal ]
   project      = var.project
   managed_zone = google_dns_managed_zone.personal.name
@@ -31,10 +42,33 @@ resource "google_dns_managed_zone_iam_member" "member" {
   member       = local.external_dns
 }
 
-resource "google_artifact_registry_repository_iam_member" "member" {
+resource "google_artifact_registry_repository_iam_member" "jenkins" {
   depends_on = [ google_artifact_registry_repository.docker_dev ]
   project      = var.project
   repository = google_artifact_registry_repository.docker_dev.name
   role         = "roles/artifactregistry.repoAdmin"
   member       = local.jenkins
+}
+
+resource "google_artifact_registry_repository_iam_member" "argocd" {
+  depends_on = [ google_artifact_registry_repository.helm_dev ]
+  project      = var.project
+  repository = google_artifact_registry_repository.helm_dev.name
+  role         = "roles/artifactregistry.repoAdmin"
+  member       = local.argocd
+}
+
+resource "google_service_account" "argocd" {
+  account_id   = "argocd"
+  display_name = "ArgoCD repo service account"
+}
+
+resource "google_project_iam_member" "artifact_registry_reader" {
+  project = var.project
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.argocd.email}"
+}
+
+resource "google_service_account_key" "argocd" {
+  service_account_id = google_service_account.argocd.name
 }
